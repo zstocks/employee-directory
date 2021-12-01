a9ebcfbe296ea44c237aa27e62b74871ed87996a
Zachary Stocks <stocks.zachary@gmail.com>
2021-12-01 10:18:08 -0700

    Refactoring for clarity. Added comments up to this point.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 1. Describe next - prev functionality. Describe how I accomplished removing the next/prev button for the last/first modal.
 2. Describe how I accomplished the formatting for the phone/dob
+3. Explain why the index variable needs global scope
--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,31 @@
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.querySelector('#gallery');
-let thisCard;
-let nextCard;
-let prevCard;
+let index;
 
-let html = `
- <form action="#" method="get">
-  <input type="search" id="search-input" class="search-input" placeholder="Search...">
-  <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
- </form>
-`;
-searchContainer.insertAdjacentHTML('beforeend', html);
-
+// Get 12 random people in the US from the RandomUser API
 fetch('https://randomuser.me/api/?results=12&nat=us')
  .then( response => response.json() )
  .then( data => {
   console.log(data.results);
   makeCards(data);
-  const employees = data.results;
 
+  // Generate an array containing all the employee cards
   let cards = document.querySelectorAll('.card');
   cards = Array.from(cards);
+
+  // Add an event listener to each card to generate a modal for the card that is clicked
   for (const card of cards) {
    card.addEventListener('click', () => {
-    thisCard = cards.indexOf(card);
-    makeModal(thisCard, data);
-
-    
-
-    
-
+    index = cards.indexOf(card);
+    makeModal(index, data);
    });
   }
-
  });
 
 /**
-* Create each employee's card using the returned API results
+* Create each employee's card using the RandomUser API results.
 * 
-* @param {object} userData - JSON response from the Random User API
+* @param {Object} userData - JSON response from the Random User API.
 */
 const makeCards = userData => {
  for (const user of userData.results) {
@@ -61,14 +48,15 @@ const makeCards = userData => {
   }
 }
 
-const makeModal = (card, data) => {
+/**
+ * Creates a modal and adds it to the page along with necessary event listeners for closing the modal or moving through the directory
+ * 
+ * @param {HTMLElement} index - the index associated with the employee data to display in the modal.
+ * @param {Object} - JSON response from the Random User API.
+ */
+const makeModal = (index, data) => {
  
- const user = data.results[card];
- console.log(user);
-
- //Format the API results for this user's phone and date of birth.
- const userPhone = `(${user.phone.slice(1, 4)}) ${user.phone.slice(6, 9)}-${user.phone.slice(-4)}`; // (xxx) xxx-xxxx
- const userDOB = `${user.dob.date.slice(5, 7)}/${user.dob.date.slice(8, 10)}/${user.dob.date.slice(0, 4)}`; // MM/DD/YYYY
+ const employee = data.results[index];
 
  // Build modal HTML
  let html = `
@@ -76,20 +64,20 @@ const makeModal = (card, data) => {
   <div class="modal">
    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
     <div class="modal-info-container">
-     <img class="modal-img" src="${user.picture.large}" alt="profile picture">
-     <h3 id="${user.name.first}-${user.name.last}-modal" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-     <p class="modal-text">${user.email}</p>
-     <p class="modal-text cap">${user.location.city}</p>
+     <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+     <h3 id="${employee.name.first}-${employee.name.last}-modal" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+     <p class="modal-text">${employee.email}</p>
+     <p class="modal-text cap">${employee.location.city}</p>
      <hr>
-     <p class="modal-text">${userPhone}</p>
-     <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
-     <p class="modal-text">Birthday: ${userDOB}</p>
+     <p class="modal-text">${formatPhone(employee)}</p>
+     <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+     <p class="modal-text">Birthday: ${formatDOB(employee)}</p>
     </div>
    </div>
 
    <div class="modal-btn-container">
-    <button type="button" id="modal-prev" class="modal-prev btn" ${thisCard === 0 ? 'style="display: none;"' : ''}>Prev</button>
-    <button type="button" id="modal-next" class="modal-next btn" ${thisCard === 11 ? 'style="display: none;"' : ''}>Next</button>
+    <button type="button" id="modal-prev" class="modal-prev btn" ${index === 0 ? 'style="display: none;"' : ''}>Prev</button>
+    <button type="button" id="modal-next" class="modal-next btn" ${index === 11 ? 'style="display: none;"' : ''}>Next</button>
    </div>
   </div>
  `;
@@ -102,18 +90,34 @@ const makeModal = (card, data) => {
 
  // When the next button is clicked, display a modal for the next employee
  document.querySelector('#modal-next').addEventListener('click', () => {
-  thisCard += 1;
+  index += 1;
   closeModal();
-  makeModal(thisCard, data);
+  makeModal(index, data);
  });
 
  // When the prev button is clicked, display a modal for the previous employee
  document.querySelector('#modal-prev').addEventListener('click', () => {
-  thisCard -= 1;
+  index -= 1;
   closeModal();
-  makeModal(thisCard, data);
+  makeModal(index, data);
  });
 }
+
+/**
+ * Converts employee's phone number to the following format: (xxx) xxx-xxxx
+ *
+ * @param (Object) employee - object containing data for this employee
+ * @returns (string) - the properly formatted phone number
+ */
+const formatPhone = employee => `(${employee.phone.slice(1, 4)}) ${employee.phone.slice(6, 9)}-${employee.phone.slice(-4)}`;
+
+/**
+ * Converts employee's date of birth to the following format: MM/DD/YYYY
+ *
+ * @param (Object) employee - object containing data for this employee
+ * @returns (string) - the properly formatted date of birth
+ */
+const formatDOB = employee => `${employee.dob.date.slice(5, 7)}/${employee.dob.date.slice(8, 10)}/${employee.dob.date.slice(0, 4)}`;
 
 /**
  * Removes the current modal from the DOM
@@ -121,3 +125,13 @@ const makeModal = (card, data) => {
 const closeModal = () => {
     document.querySelector('.modal-container').remove();
 }
+
+// Add search element to the DOM
+let html = `
+ <form action="#" method="get">
+  <input type="search" id="search-input" class="search-input" placeholder="Search...">
+  <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+ </form>
+`;
+
+searchContainer.insertAdjacentHTML('beforeend', html);
